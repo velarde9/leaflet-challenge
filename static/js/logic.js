1cc3a33ee2406191c62715a0c30121b0c1e40322
velarde9 <velarde.donna.camille@gmail.com>
2023-12-03 20:45:59 -0500

    final changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,7 +12,7 @@ let myMap = L.map("map", {
 // Store our API endpoint as queryUrl.
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Perform a GET request to the query URL/
+// Perform GET request to queryUrl
 d3.json(queryUrl).then(function (data) {
     
     console.log(data)
